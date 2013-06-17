a2e9c35b12ce6511b9cb88724f107c51297970d8
nathan lachenmyer <asymptoticdesign@mit.edu>
2013-06-16 20:04:48 -0400

    First commit

---
+++ src/pendulumWaves.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "ofMain.h"
+
+class pendulumWaves : public ofBaseApp{
+ public:
+  void setup();
+  void update();
+  void draw();
+		
+  void keyPressed(int key);
+  void keyReleased(int key);
+  void mouseMoved(int x, int y);
+  void mouseDragged(int x, int y, int button);
+  void mousePressed(int x, int y, int button);
+  void mouseReleased(int x, int y, int button);
+  void windowResized(int w, int h);
+  void dragEvent(ofDragInfo dragInfo);
+  void gotMessage(ofMessage msg);
+
+  struct Pendulum {
+    int index;
+    float freq;
+    float pos;
+    float vel;
+    float hit;
+  };
+  
+  float width, height;
+  static const int numPendulums = 20;
+  Pendulum pendulums[numPendulums];
+  float startTime;
+};
